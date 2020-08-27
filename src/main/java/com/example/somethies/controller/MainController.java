2dfb9c2ed91d7a5fa8411c8460f6fae4282d406e
SUGAR2TWO <sugar22two@gmail.com>
2020-08-27 14:37:39 +0300

    bootystrap and some shit

--- src/main/java/com/example/somethies/controller/MainController.java
+++ src/main/java/com/example/somethies/controller/MainController.java
@@ -33,7 +33,6 @@ public class MainController {
 
     @GetMapping("/main")
     public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
-
         Iterable<Message> messages = messageRepo.findAll();
 
         if(filter != null && !filter.isEmpty()) {
