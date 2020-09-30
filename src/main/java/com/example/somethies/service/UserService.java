7ba3c45a60f73775725695ec449b8ddaf728d4f9
SUGAR2TWO <sugar22two@gmail.com>
2020-09-30 16:32:24 +0300

    profile and stuff

--- src/main/java/com/example/somethies/service/UserService.java
+++ src/main/java/com/example/somethies/service/UserService.java
@@ -10,16 +10,13 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepo userRepo;
-
-    /*@Autowired
-    private SmtpMailSender smtpMailSender;*/
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
@@ -35,35 +32,39 @@ public class UserService implements UserDetailsService {
 
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
-        //user.setActivationCode(UUID.randomUUID().toString());
 
         userRepo.save(user);
-
-        /*if (!StringUtils.isEmpty(user.getEmail())){
-            String message = String.format(
-              "Hello, %s! \n" +
-                      "Welcome to Somethies. Please, visit next link: http://localhost:8080/activate/%s",
-                    user.getUsername(),
-                    user.getActivationCode()
-            );
-
-            smtpMailSender.send(user.getEmail(), "Activation code", message);
-        }*/
 
         return true;
     }
 
-    /*public boolean activateUser(String code) {
-        User user = userRepo.findByActivationCode(code);
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
 
-        if(user == null){
-            return false;
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()){
+            if (roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
         }
 
-        user.setActivationCode(null);
+        userRepo.save(user);
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void updateProfile(User user, String password) {
+        if (!StringUtils.isEmpty(password)) {
+            user.setPassword(password);
+        }
 
         userRepo.save(user);
-
-        return true;
-    }*/
+    }
 }
